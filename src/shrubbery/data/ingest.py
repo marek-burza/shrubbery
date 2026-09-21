62475d017e10a0abd9f55a6529a5f961183d01b0
marek-burza <13467877+marek-burza@users.noreply.github.com>
2026-09-21 10:49:25 +0200

    Removed some unused code

--- src/shrubbery/data/ingest.py
+++ src/shrubbery/data/ingest.py
@@ -1,4 +1,3 @@
-import hashlib
 import json
 import math
 from pathlib import Path
@@ -10,65 +9,22 @@ from shrubbery.constants import COLUMN_ERA
 from shrubbery.data.augmentation import numeric_eras
 from shrubbery.napi import napi
 from shrubbery.observability import logger
-from shrubbery.workspace import get_workspace_path
-
-DATA_SUBDIRECTORY = 'data'
 
 
-# Tournament data changes every week so we specify the round in their name.
-# Training and validation data only change periodically, so no need to
-# download them every time.
-FILES_TO_DOWNLOAD = {
-    'train.parquet': {'investigate': True},
-    'validation.parquet': {'investigate': False},
-    'live.parquet': {'investigate': False},
-    'features.json': {'investigate': False},
-}
+def locate_numerai_file(file_name: str) -> Path:
+    data_directory_path = Path('./workspace') / 'data'
+    data_directory_path.mkdir(parents=True, exist_ok=True)
+    file_path = data_directory_path / file_name
+    return file_path
 
 
-def fully_qualify_file_name(
-    directory: Path, file_name: str
-) -> tuple[Path, Path]:
-    previous_file_name = file_name
-    current_file_name = file_name
-    return directory / current_file_name, directory / previous_file_name
-
-
-def file_hash(file_path: Path) -> str | None:
-    try:
-        with open(file_path, 'rb') as handle:
-            file_hash_object = hashlib.sha512()
-            while chunk := handle.read(1048576):
-                file_hash_object.update(chunk)
-            return file_hash_object.hexdigest()
-    except Exception:
-        return None
-
-
-def download_file_and_investigate(
-    file_name: str,
-    investigate: bool = False,
-) -> bool | None:
-    different = None
-    data_directory_path = get_workspace_path(DATA_SUBDIRECTORY)
-    current_file_path, previous_file_path = fully_qualify_file_name(
-        data_directory_path, file_name
-    )
-    logger.info(f'Downloading {file_name} to {current_file_path}')
-    if investigate:
-        previous_file_hash = file_hash(previous_file_path)
-    napi.download_dataset(f'v5.3/{file_name}', str(current_file_path))
+def download_file(file_name: str) -> None:
+    file_path = locate_numerai_file(file_name)
+    logger.info(f'Downloading {file_name} to {file_path}')
+    napi.download_dataset(f'v5.3/{file_name}', str(file_path))
     if file_name.endswith('.parquet'):
-        length = len(pd.read_parquet(current_file_path, columns=[]))
+        length = len(pd.read_parquet(file_path, columns=[]))
         logger.info(f'File {file_name} has {length} rows')
-    if investigate:
-        current_file_hash = file_hash(current_file_path)
-        different = (
-            current_file_hash != previous_file_hash
-            if None not in [current_file_hash, previous_file_hash]
-            else None
-        )
-    return different
 
 
 def log_all_dataset_entries() -> None:
@@ -81,21 +37,16 @@ def log_all_dataset_entries() -> None:
 def download_numerai_files():
     log_all_dataset_entries()
     logger.info('Downloading dataset files...')
-    for file_name in FILES_TO_DOWNLOAD:
-        investigate = FILES_TO_DOWNLOAD[file_name]['investigate']
-        different = download_file_and_investigate(file_name, investigate)
-        if different is not None and different:
-            logger.warning(f'Warning - changed file content of {file_name}')
-
-
-def locate_numerai_file(file_name: str) -> Path:
-    data_directory_path = get_workspace_path(DATA_SUBDIRECTORY)
-    file_path, _ = fully_qualify_file_name(data_directory_path, file_name)
-    return file_path
+    for file_name in [
+        'train.parquet',
+        'validation.parquet',
+        'live.parquet',
+        'features.json',
+    ]:
+        download_file(file_name)
 
 
 def get_feature_set(selected_feature_set: str) -> list[str]:
-    # Read the feature metadata and get a feature set
     with open(locate_numerai_file('features.json'), 'r') as handle:
         feature_metadata = json.load(handle)
     all_features = set()
@@ -117,9 +68,10 @@ def read_parquet_and_unpack(
     data = pd.read_parquet(
         locate_numerai_file(file_name), columns=read_columns
     )
-    # For more information about int8 encoding, see: https://forum.numer.ai/t/rain-data-release/6657  # noqa: E501
+    # For more information about int8 encoding, see:
+    # https://forum.numer.ai/t/rain-data-release/6657
     data[feature_cols] = (
-        data[feature_cols].apply(lambda x: x / 4.0).astype(np.float16)
+        data[feature_cols].apply(lambda x: x / 4.0).astype(np.float32)
     )
     data_column_era = data[COLUMN_ERA]
     data_column_era = np.where(data_column_era == 'X', np.nan, data_column_era)
